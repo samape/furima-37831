16ff8c41e1e80f4e4724c8c9806162e0fb53cf6e
samape <s.sakamoto.613@gmail.com>
2022-08-01 23:37:34 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,         presence: true
-  validates :first_name,       presence: true
-  validates :family_name,      presence: true
-  validates :first_name_kana,  presence: true
-  validates :family_name_kana, presence: true
-  validates :birthday,         presence: true
+  with_options presence: true do
+    validates :nickname
+    # 全角（漢字・ひらがな・カタカナ）
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    # 全角（漢字・ひらがな・カタカナ）
+    validates :family_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    # 全角（カタカナ）
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ]+\z/ }
+    # 全角（カタカナ）
+    validates :family_name_kana, format: {with: /\A[ァ-ヶ]+\z/ }
+    # 生年月日
+    validates :birthday
+  end
 end
