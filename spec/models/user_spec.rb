d802e22fd0f09303dc2207ae8435f9a85e682e5b
samape <s.sakamoto.613@gmail.com>
2022-08-11 21:49:28 +0900

    モデル単体テスト実装

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,99 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    context '内容に問題がない場合' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'nicknameが空だと保存できないこと' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空だと保存できないこと' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it '既に登録されているemailは保存できないこと' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'emailに@が含まれていないと保存できないこと' do
+        @user.email = 'testtest.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'passwordが空だと保存できないこと' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordは6文字以上でないと保存できないこと' do
+        @user.password = 'test1'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordは半角英数字混合でないと保存できないこと' do
+        @user.password = '111111'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is invalid")
+      end
+      it 'passwordとpassword確認は値が異なると保存できないこと' do
+        @user.password = 'test123'
+        @user.password_confirmation = 'test456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'お名前の名字が空だと保存できないこと' do
+        @user.family_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name can't be blank")
+      end
+      it 'お名前の名前が空だと保存できないこと' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it 'お名前は全角(漢字・ひらがな・カタカナ)でないと保存できないこと' do
+        @user.family_name = 'Tanaka'
+        @user.first_name = 'Kenji'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name is invalid", "Family name is invalid")
+      end
+      it 'お名前カナは名字が空だと保存できないこと' do
+        @user.family_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+      end
+      it 'お名前カナは名前が空だと保存できないこと' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+      end
+      it 'お名前カナは全角(カタカナ)でないと保存できないこと' do
+        @user.family_name_kana = 'たなか'
+        @user.first_name_kana = 'けんじ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana is invalid", "Family name kana is invalid")
+      end
+      it '生年月日が空だと保存できないこと' do
+        @user.birthday = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
+    end
+  end
+end
