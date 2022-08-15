7b80dad1518ddbf84f44f2aaeb22ddfa1b15e645
samape <s.sakamoto.613@gmail.com>
2022-08-15 23:22:19 +0900

    出品機能の実装
    コントローラーとitem/new.html.erb実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-  
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
-  
+
+  private
+  def item_params
+    params.require(:item).permit(
+      :name, :content, :category_id, :status_id, :del_charge_id, :prefecture_id, :del_day_id, :price, :user
+    )
+  end
 end
