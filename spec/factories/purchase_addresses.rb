2f50b463eb854da5530346d8055f82aef4b1bc62
samape <s.sakamoto.613@gmail.com>
2022-08-31 12:57:47 +0900

    モデル単体テスト実装

---
+++ spec/factories/purchase_addresses.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :purchase_address do
+    token { 'tok_abcdefghijk00000000000000000' }
+    post_code     { '111-1111' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city          { '中野区' }
+    address       { '中野3-3-3' }
+    building      { 'ライオンズマンション101' }
+    phone_num     { '09011111111' }
+  end
+end
